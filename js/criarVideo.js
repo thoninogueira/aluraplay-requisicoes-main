054eceee80eb3f2bb24a204e76221ca4fd3ce901
Thoni Nogueira <an98238@gmail.comlocalhost:3000/videos>
2024-06-29 06:29:13 -0300

    commit

---
+++ js/criarVideo.js
@@ -0,0 +1,7 @@
+const formulario = document.querySelector("[data-formulario]");
+
+function criaVideo() {
+    const imagem = document.querySelector("[data-imagem]");
+    const url = document.querySelector("[data-url]");
+    const titulo = document.querySelector("[data-titulo]");
+}
